55d462bbed0d27d6d590ed279f16e2bdb1cf988b
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-10-18 18:06:12 -0300

    initial commit

---
+++ consomeAPI/src/test/java/edu/patrick/consomeAPI/ConsomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.patrick.consomeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
